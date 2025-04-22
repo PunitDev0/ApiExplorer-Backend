a891c7c4b5a12f773cc44254139e3b31bcaa4eb6
PunitDev0 <punitdeveloper1@gmail.com>
2025-04-22 23:56:36 +0530

    try to fixed cookies error

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -46,7 +46,7 @@ const registerUser = asyncHandler(async (req, res) => {
     // Set cookie
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
@@ -113,7 +113,7 @@ const loginUser = asyncHandler(async (req, res) => {
     // Set cookie
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
@@ -143,6 +143,8 @@ const loginUser = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   try {
     const token = req.cookies.token;
+    console.log(token);
+    
     if (!token) {
       return res.status(401).json({ 
         success: false,
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -104,7 +104,7 @@ router.route("/github/callback").get(
 
       res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
